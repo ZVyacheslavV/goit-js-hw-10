07f367769cbfb42bda749b45ac8b3bbd0120c384
ZVyacheslavV <vvvv.zkv@gmail.com>
2025-07-28 16:04:44 +0000

    Deploying to gh-pages from @ ZVyacheslavV/goit-js-hw-10@8c5d33cc45e59347ecb061163e174536592a46a3 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */var e;const t={state:(e=document.querySelector('input[name="state"]:checked'))==null?void 0:e.value,notifyBtn:document.querySelector(".submit-btn")};t.notifyBtn.addEventListener("click",console.log(t.state));
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-BbbuE1sJ.js";var e;const t={state:(e=document.querySelector('input[name="state"]:checked'))==null?void 0:e.value,notifyBtn:document.querySelector(".submit-btn")};t.notifyBtn.addEventListener("click",console.log(t.state));
 //# sourceMappingURL=2-snackbar.js.map
