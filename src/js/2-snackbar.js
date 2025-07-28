8b3fd30174eba8c869fcf8f6ebff503794db39bb
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-07-28 19:59:30 +0300

    finish second task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,8 +2,38 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const refs = {
-  state: document.querySelector('input[name="state"]:checked')?.value,
-  notifyBtn: document.querySelector('.submit-btn'),
+  form: document.querySelector('.form'),
 };
 
-refs.notifyBtn.addEventListener('click', console.log(refs.state));
+const makePromise = (delay, state) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      state === 'fulfilled' ? resolve(delay) : reject(delay);
+    }, delay);
+  });
+};
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  const delay = +refs.form.elements.delay.value;
+  const state = refs.form.elements.state.value;
+
+  makePromise(delay, state)
+    .then(delay =>
+      iziToast.success({
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 2000,
+        progressBar: false,
+      })
+    )
+    .catch(delay =>
+      iziToast.error({
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 2000,
+        progressBar: false,
+      })
+    );
+  refs.form.reset();
+});
