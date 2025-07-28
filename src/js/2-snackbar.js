8c5d33cc45e59347ecb061163e174536592a46a3
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-07-28 19:04:30 +0300

    add min-heigth to fieldset for same display in both browsers

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,6 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const refs = {
   state: document.querySelector('input[name="state"]:checked')?.value,
   notifyBtn: document.querySelector('.submit-btn'),
